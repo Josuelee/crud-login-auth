c916e725e7d7e27d36f054af02df738db6d79c03
Josuelee <josuelee28@gmail.com>
2022-06-03 22:11:36 -0600

    seo url

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -4,25 +4,36 @@ import socialImage from "../assets/create.jpeg";
 const Seo = () => {
   return (
   <Helmet>
-    <meta
-      name="description"
-      content="Full Stack application to make a Login & CRUD"
-    />
+
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://crud-login-auth.netlify.app/" />
     <meta property="og:title" content="Crud-Login-Auth" />
     <meta
       property="og:description"
       content="Full Stack application to make a Login & CRUD"
-    />  
-    <meta property="og:image" content={socialImage} />  
+    />
+    <meta
+      property="og:image"
+      content={`https://crud-login-auth.netlify.app${socialImage}`}
+    />
+
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta
+      property="twitter:url"
+      content="https://crud-login-auth.netlify.app/"
+    />
+    <meta property="twitter:title" content="Crud-Login-Auth" />
+    <meta
+      property="twitter:description"
+      content="Full Stack application to make a Login & CRUD"
+    />
+    <meta
+      property="twitter:image"
+      content={`https://crud-login-auth.netlify.app${socialImage}`}
+    />
   </Helmet>
   )
 }
 export default Seo
 
 
-
-/* 
-
-  
-
-*/
